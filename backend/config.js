813df78ecf833e3f71afa383be6500cac38f27f2
Himanshi Singh <himanshi.10711661@ltimindtree.com>
2024-03-06 22:01:15 +0530

    adding spinner and Home component

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
-export const PORT = 3466;
+export const PORT = 5555;
 export const mongoDbUrl = 'mongodb+srv://himanshisingh:root@bookstore.iaqkvkj.mongodb.net/books-collection?retryWrites=true&w=majority'
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [books,setbooks] =useState([]);
+  const [loading,setisloading] =useState(false);
+  useEffect(() => {
+    setisloading(true)
+    axios.get('http://localhost:5555/books')
+    .then((res)=>{
+      setbooks(res.data.data);
+      setisloading(false);
+    })
+    .catch((err)=>{
+      console.log(err);
+      setisloading(false);
+    })
+    
+  }, [])
+  
   return (
     <div>Home</div>
   )
